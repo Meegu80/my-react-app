017207c69aea99e49cc6f7c62bc254467596e95b
Meegu80 <aceman80@nate.com>
2025-10-30 11:34:47 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <section style={{ width: '100vw', backgroundColor: 'yellow', textAlign: 'center' }}>
+      <h1>Vite + React 설치 완료!</h1>
+      <p>동해물과 백주산이 마르고 </p>
+    </section>
+  )
+}
+
+export default App
